1005316b6727be136cf8a8389771a64eed518837
Kotaro Kuroda <kuroda.k.ak@m.titech.ac.jp>
2019-05-11 19:08:03 +0900

    ボタンを作った

--- a.js
+++ a.js
@@ -1,1 +1,13 @@
-console.log("test")
+const button = document.getElementById("button")
+const good = document.getElementById("good")
+let i = 0;
+button.addEventListener("click", ()=>{
+    i += 1
+    if(i % 2 == 0){
+        good.textContent = "偶数"
+    }else{
+        good.textContent = "奇数"
+    }
+    
+    console.log("test")
+})
