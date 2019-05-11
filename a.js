c8e79022e20c48b7731aada944456d37f6b052ab
Kotaro Kuroda <kuroda.k.ak@m.titech.ac.jp>
2019-05-11 19:09:40 +0900

    ボタンの機能を修正した

--- a.js
+++ a.js
@@ -2,12 +2,12 @@ const button = document.getElementById("button")
 const good = document.getElementById("good")
 let i = 0;
 button.addEventListener("click", ()=>{
-    i += 1
+    console.log(i)
     if(i % 2 == 0){
         good.textContent = "偶数"
     }else{
         good.textContent = "奇数"
     }
-    
+    i += 1
     console.log("test")
 })
