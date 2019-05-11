b9af7df37ccfce03a7a5ac88f19bafc080ab5e47
Kotaro Kuroda <kuroda.k.ak@m.titech.ac.jp>
2019-05-11 19:16:27 +0900

    表示される文を変えた

--- a.js
+++ a.js
@@ -5,9 +5,9 @@ button.addEventListener("click", ()=>{
     i += 1
     console.log(i)
     if(i % 2 == 0){
-        good.textContent = "偶数"
+        good.textContent = "偶数回押しました"
     }else{
-        good.textContent = "奇数"
+        good.textContent = "奇数回押しました"
     }
     i += 1
     console.log("test")
